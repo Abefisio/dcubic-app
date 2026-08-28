fee500d38c4f448ef286e10cdf38d9647b87f7ed
Yujiro Abe <abedrdador@gmail.com>
2026-08-28 08:52:06 -0300

    style: aumentar título do login em 50% e reduzir largura do formulário em 50%

--- app.py
+++ app.py
@@ -195,6 +195,7 @@ st.markdown(
     "<style>"
     ".block-container{padding-top:0rem !important;}"
     "header[data-testid='stHeader']{height:0;}"
+    "[data-testid='stForm']{max-width:50% !important;margin-left:auto !important;margin-right:auto !important;}"
     "</style>",
     unsafe_allow_html=True,
 )
